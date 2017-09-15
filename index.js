04f435030318579dbab72968223fc9d5c8347798
zhuchentong <2037630@163.com>
2017-09-15 16:39:19 +0800

    udpate

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import app from './src/config/app.config'
+const launchPage = 'home.html'
 
 const ready = function () {
   // 设置状态条
@@ -8,7 +9,7 @@ const ready = function () {
   // 打开页面
   window.api.openFrame({
     name: 'index',
-    url: app.debug ? 'http://192.168.3.89:8010/home.html' : './home.html', // 调试时使用
+    url: app.debug ? `${app.remote}/${launchPage}` : `./${launchPage}`, // 调试时使用
     rect: {
       x: 0,
       y: 45,
